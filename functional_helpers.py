a6774ef4f5325d0fba3bb2a92e5e03b557f3e450
Mikey Mo <mikeyjkmo@gmail.com>
2020-11-02 18:10:24 +0000

    Add more tests and use functool.wraps

--- functional_helpers.py
+++ functional_helpers.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from functools import reduce, wraps
 from inspect import isawaitable
 
 
@@ -9,17 +9,25 @@ async def _ensure_awaitable(res):
 
 
 def async_pipe(*fns):
+    assert len(fns) > 1, "There must be at least 2 functions to pipe"
+
+    @wraps(fns[0])
     def new_fn(initial_input=None):
         async def step(acc, f):
             prev_result = await _ensure_awaitable(acc)
             return await _ensure_awaitable(f(prev_result))
         return reduce(step, fns, initial_input)
+
     return new_fn
 
 
 def pipe(*fns):
+    assert len(fns) > 1, "There must be at least 2 functions to pipe"
+
+    @wraps(fns[0])
     def new_fn(initial_input=None):
         def step(acc, f):
             return f(acc)
         return reduce(step, fns, initial_input)
+
     return new_fn
--- test_functional_helpers.py
+++ test_functional_helpers.py
@@ -1,7 +1,11 @@
 from typing import NamedTuple, Optional
 
 import pytest
-from functional_helpers import async_pipe
+from functional_helpers import async_pipe, pipe
+
+
+class CustomException(Exception):
+    pass
 
 
 class PipelineData(NamedTuple):
@@ -9,15 +13,31 @@ class PipelineData(NamedTuple):
     value: Optional[int]
 
 
-async def get_value(param: PipelineData):
+async def get_value_async(param: PipelineData):
     return PipelineData(name=param.name, value=1)
 
 
-async def get_name(param: PipelineData):
+async def get_name_async(param: PipelineData):
     return PipelineData(name="Bob", value=param.value)
 
 
-def non_async_upper_name(param: PipelineData):
+async def bad_func_async(param: PipelineData):
+    raise CustomException("This is a bad function")
+
+
+def get_value(param: PipelineData):
+    return PipelineData(name=param.name, value=1)
+
+
+def get_name(param: PipelineData):
+    return PipelineData(name="Bob", value=param.value)
+
+
+def bad_func(param: PipelineData):
+    raise CustomException("This is a bad function")
+
+
+def upper_name(param: PipelineData):
     return PipelineData(
         name=param.name.upper(),
         value=param.value,
@@ -28,17 +48,52 @@ def non_async_upper_name(param: PipelineData):
 class TestAsyncPipe:
     async def test_works_with_async_funcs(self):
         result = await async_pipe(
-            get_value,
-            get_name,
+            get_value_async,
+            get_name_async,
         )(PipelineData(name=None, value=None))
 
         assert result == PipelineData(name="Bob", value=1)
 
     async def test_works_with_mix_of_async_and_sync(self):
         result = await async_pipe(
-            get_value,
-            get_name,
-            non_async_upper_name,
+            get_value_async,
+            get_name_async,
+            upper_name,
         )(PipelineData(name=None, value=None))
 
         assert result == PipelineData(name="BOB", value=1)
+
+    async def test_raises_exception(self):
+        with pytest.raises(CustomException):
+            await async_pipe(
+                get_value_async,
+                get_name_async,
+                bad_func_async,
+            )(PipelineData(name=None, value=None))
+
+    async def test_cannot_pipe_with_no_functions(self):
+        with pytest.raises(AssertionError):
+            await async_pipe()(PipelineData(name=None, value=None))
+
+
+@pytest.mark.asyncio
+class TestPipe:
+    async def test_pipes_correctly(self):
+        result = pipe(
+            get_value,
+            get_name,
+        )(PipelineData(name=None, value=None))
+
+        assert result == PipelineData(name="Bob", value=1)
+
+    async def test_raises_exception(self):
+        with pytest.raises(CustomException):
+            await async_pipe(
+                get_value,
+                get_name,
+                bad_func,
+            )(PipelineData(name=None, value=None))
+
+    async def test_cannot_pipe_with_no_functions(self):
+        with pytest.raises(AssertionError):
+            await pipe()(PipelineData(name=None, value=None))
