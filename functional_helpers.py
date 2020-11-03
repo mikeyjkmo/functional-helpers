93da7e39f3109814b1d24e563803ccce7b122021
Mikey Mo <mikeyjkmo@gmail.com>
2020-11-03 08:44:21 +0000

    Fully typed implementations of Pipe and AsyncPipe

--- functional_helpers.py
+++ functional_helpers.py
@@ -1,33 +1,37 @@
-from functools import reduce, wraps
+from typing import Awaitable, Any, TypeVar, cast, Callable, Generic, Union
+from functools import reduce
 from inspect import isawaitable
 
 
-async def _ensure_awaitable(res):
+T = TypeVar("T")
+D = TypeVar("D")
+MaybeAwaitable = Union[D, Awaitable[D]]
+
+
+async def _ensure_awaitable(res: T) -> T:
     if isawaitable(res):
-        return await res
+        return await cast(Awaitable[T], res)
     return res
 
 
-def async_pipe(*fns):
-    assert len(fns) > 1, "There must be at least 2 functions to pipe"
+class AsyncPipe(Generic[D]):
+    def __init__(self, *fns: Callable[[D], MaybeAwaitable[D]]):
+        assert len(fns) > 1, "There must be at least 2 functions to pipe"
+        self.fns = fns
 
-    @wraps(fns[0])
-    def new_fn(initial_input):
-        async def step(acc, f):
+    def __call__(self, initial_input: D) -> Awaitable[D]:
+        async def step(acc: Any, f: Any) -> D:
             prev_result = await _ensure_awaitable(acc)
             return await _ensure_awaitable(f(prev_result))
-        return reduce(step, fns, initial_input)
-
-    return new_fn
+        return reduce(step, self.fns, cast(Awaitable[D], initial_input))
 
 
-def pipe(*fns):
-    assert len(fns) > 1, "There must be at least 2 functions to pipe"
+class Pipe(Generic[D]):
+    def __init__(self, *fns: Callable[[D], D]):
+        assert len(fns) > 1, "There must be at least 2 functions to pipe"
+        self.fns = fns
 
-    @wraps(fns[0])
-    def new_fn(initial_input):
-        def step(acc, f):
+    def __call__(self, initial_input: D) -> D:
+        def step(acc: Any, f: Any) -> Any:
             return f(acc)
-        return reduce(step, fns, initial_input)
-
-    return new_fn
+        return reduce(step, self.fns, initial_input)
--- test_functional_helpers.py
+++ test_functional_helpers.py
@@ -1,7 +1,7 @@
 from typing import NamedTuple, Optional
 
 import pytest
-from functional_helpers import async_pipe, pipe
+from functional_helpers import Pipe, AsyncPipe
 
 
 class CustomException(Exception):
@@ -13,87 +13,88 @@ class PipelineData(NamedTuple):
     value: Optional[int]
 
 
-async def get_value_async(param: PipelineData):
+async def get_value_async(param: PipelineData) -> PipelineData:
     return PipelineData(name=param.name, value=1)
 
 
-async def get_name_async(param: PipelineData):
+async def get_name_async(param: PipelineData) -> PipelineData:
     return PipelineData(name="Bob", value=param.value)
 
 
-async def bad_func_async(param: PipelineData):
+async def bad_func_async(param: PipelineData) -> PipelineData:
     raise CustomException("This is a bad function")
 
 
-def get_value(param: PipelineData):
+def get_value(param: PipelineData) -> PipelineData:
     return PipelineData(name=param.name, value=1)
 
 
-def get_name(param: PipelineData):
+def get_name(param: PipelineData) -> PipelineData:
     return PipelineData(name="Bob", value=param.value)
 
 
-def bad_func(param: PipelineData):
+def bad_func(param: PipelineData) -> PipelineData:
     raise CustomException("This is a bad function")
 
 
-def upper_name(param: PipelineData):
+def upper_name(param: PipelineData) -> PipelineData:
     return PipelineData(
-        name=param.name.upper(),
+        name=param.name.upper() if param.name else None,
         value=param.value,
     )
 
 
 @pytest.mark.asyncio
 class TestAsyncPipe:
-    async def test_works_with_async_funcs(self):
-        result = await async_pipe(
+    async def test_works_with_async_funcs(self) -> None:
+        result = await AsyncPipe(
             get_value_async,
             get_name_async,
         )(PipelineData(name=None, value=None))
 
         assert result == PipelineData(name="Bob", value=1)
 
-    async def test_works_with_mix_of_async_and_sync(self):
-        result = await async_pipe(
+    async def test_works_with_mix_of_async_and_sync(self) -> None:
+        result = await AsyncPipe(
             get_value_async,
             get_name_async,
             upper_name,
         )(PipelineData(name=None, value=None))
-
         assert result == PipelineData(name="BOB", value=1)
 
-    async def test_raises_exception(self):
+    async def test_raises_exception(self) -> None:
         with pytest.raises(CustomException):
-            await async_pipe(
+            await AsyncPipe(
                 get_value_async,
                 get_name_async,
                 bad_func_async,
             )(PipelineData(name=None, value=None))
 
-    async def test_cannot_pipe_with_no_functions(self):
+    async def test_cannot_pipe_with_no_functions(self) -> None:
         with pytest.raises(AssertionError):
-            await async_pipe()(PipelineData(name=None, value=None))
+            await AsyncPipe[PipelineData]()(
+                PipelineData(name=None, value=None)
+            )
 
 
 @pytest.mark.asyncio
 class TestPipe:
-    async def test_pipes_correctly(self):
-        result = pipe(
+    async def test_pipes_correctly(self) -> None:
+        result = Pipe(
             get_value,
             get_name,
         )(PipelineData(name=None, value=None))
 
         assert result == PipelineData(name="Bob", value=1)
 
-    async def test_raises_exception(self):
+    async def test_raises_exception(self) -> None:
         with pytest.raises(CustomException):
-            await async_pipe(
+            Pipe(
                 get_value,
                 get_name,
                 bad_func,
             )(PipelineData(name=None, value=None))
 
-    async def test_cannot_pipe_with_no_functions(self):
+    async def test_cannot_pipe_with_no_functions(self) -> None:
         with pytest.raises(AssertionError):
-            await pipe()(PipelineData(name=None, value=None))
+            Pipe()(PipelineData(name=None, value=None))  # type: ignore
